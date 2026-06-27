df640e9430826fcecaa108673df0120a4d664689
= <hb24mcf1r14@student.nitw.ac.in>
2026-06-27 13:42:48 +0530

    Added cors functionality

--- exp-LAST/app.js
+++ exp-LAST/app.js
@@ -1,8 +1,9 @@
 const express=require("express")
 const config=require("./config/config")
 const app=express()
-
+const cors = require('cors')
 app.use(express.json())
+app.use(cors())
 
 app.use(
 "/api",
@@ -25,3 +26,39 @@ console.log(
 
 )
 
+//CORS --CROSS  ORIGIN RESOURCE SHARING
+// Behind the scenes:
+
+// Ye middleware har response me header add karta hai:
+
+// Access-Control-Allow-Origin: *
+// Meaning:
+// "Any orIgin allowed."
+
+// Example:
+// Request:GET /users
+
+// Origin: http://localhost:5173
+
+// Express:
+
+// Response:
+// Access-Control-Allow-Origin: *
+// Browser:allowed.
+
+//EXMPALE FOR ALLWONING SPECIFIC FRONTENDS WITH RESTRICTED METHODS
+
+// const allowed = [
+//  "https://app.com",
+//  "https://admin.app.com"
+// ]
+
+
+// app.use(cors({
+//  origin: allowed,
+//  methods:[
+//   "GET",
+//   "POST"
+//  ]
+
+// }))
